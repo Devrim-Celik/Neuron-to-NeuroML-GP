0c7a1da86eadafa5965e0e1900e987921d539f9c
Devrim Celik <DevCel@hotmail.de>
2018-01-29 14:55:15 +0000

    adjusted to changes

--- complete.py
+++ complete.py
@@ -2,11 +2,14 @@ from auxiliary import trace_to_np, unpack, generate_plots
 from extract_df import get_gate_values
 from sigmoidal_estimator import Sigmoidal_Estimator
 from dim_reduction import dim_reduction
+from analysis import analysis
 
-names, groups, traces = unpack()
 
 get_gate_values()
-generate_plots()
-dim_reduction(names, groups, traces)
-SE = Sigmoidal_Estimator(names[5], groups[5], traces[5])
+names, ion_types, groups, traces = unpack()
+
+generate_plots(names, ion_types, groups, traces)
+dim_reduction(names, ion_types, groups, traces)
+SE = Sigmoidal_Estimator(names[5], ion_types[5], groups[5], traces[5])
 SE.routine()
+analysis()
