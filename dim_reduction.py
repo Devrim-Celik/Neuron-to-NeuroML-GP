1a4ac25dc922a8964bf514664cedb6c1d1d1fb46
Devrim Celik <DevCel@hotmail.de>
2018-01-29 14:54:01 +0000

    creates 2 plots, clustering colored by groups/ion_types; also creates manually 2 legends to copy & paste

--- dim_reduction.py
+++ dim_reduction.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import pandas as pd
 from tsne import bh_sne
 from sklearn.decomposition import PCA
@@ -7,8 +8,16 @@ from sklearn.cluster import KMeans
 from mpldatacursor import datacursor
 from auxiliary import unpack
 
-def dim_reduction(names, groups, traces, reduced_dim=2, perplexity=15,
+# assign each group a color
+group_to_color = {"PS": "red", "NS": "blue", "G": "green", "F": "black"}
+# assign each group a color
+type_to_color = {"Na": "blue", "K": "red", "Ca": "green"}
+
+def dim_reduction(names, ion_types, groups, traces, reduced_dim=2, perplexity=15,
     use_tSNE=True):
+
+    # marker size
+    ms = 15
 
     if use_tSNE:
         data2d = bh_sne(traces, d=reduced_dim, perplexity=perplexity)
@@ -16,23 +25,62 @@ def dim_reduction(names, groups, traces, reduced_dim=2, perplexity=15,
         pca = PCA(n_components=reduced_dim)
         data2d = pca.fit_transform(traces)
 
-    # assign each group a color
-    group_to_color = {"PS": "red", "NS": "blue", "G": "green", "F": "black"}
 
-    plt.figure(figsize=(20,10))
+
+    plt.figure("By_Group", figsize=(20,10))
 
     for nr, dp in enumerate(data2d):
-        plt.scatter(dp[0], dp[1], label=names[nr], c=group_to_color[groups[nr]])
+        plt.scatter(dp[0], dp[1], label=names[nr], \
+        c=group_to_color[groups[nr]], s=ms)
 
     datacursor(formatter='{label}'.format, bbox=dict(fc='white'),       \
                  arrowprops=dict(arrowstyle='simple', fc='black', alpha=0.5))
 
-    plt.savefig("plots/clustering.png")
-    plt.show()
+    plt.savefig("plots/clustering_group.png")
+
+
+
+
+
+    plt.figure("By_Type", figsize=(20,10))
+
+    for nr, dp in enumerate(data2d):
+        plt.scatter(dp[0], dp[1], label=names[nr], \
+        c=type_to_color[ion_types[nr]], s=ms)
+
+    datacursor(formatter='{label}'.format, bbox=dict(fc='white'),       \
+                 arrowprops=dict(arrowstyle='simple', fc='black', alpha=0.5))
+
+    plt.savefig("plots/clustering_type.png")
+
+
+
+def create_legends():
+    """
+    create legends for the clustering plots, since you have to choose to either
+    add a legend or make the plot interactive
+    """
+
+    plt.figure("Leg1", figsize=(20,10))
+    patch1 = mpatches.Patch(color=group_to_color["PS"], label="Positive Sigmoid")
+    patch2 = mpatches.Patch(color=group_to_color["NS"], label="Negative Sigmoid")
+    patch3 = mpatches.Patch(color=group_to_color["G"], label="Gaussian")
+    patch4 = mpatches.Patch(color=group_to_color["F"], label="Flawed")
+    plt.legend(loc="upper left", handles=[patch1, patch2, patch3, patch4], prop={'size': 60})
+    plt.savefig("plots/clustering_group_legend.png")
+
+    plt.figure("Leg2", figsize=(20,10))
+    patch1 = mpatches.Patch(color=type_to_color["Na"], label="Sodium(Na)")
+    patch2 = mpatches.Patch(color=type_to_color["K"], label="Potassium(K)")
+    patch3 = mpatches.Patch(color=type_to_color["Ca"], label="Calcium(Ca)")
+    plt.legend(loc="upper left", handles=[patch1, patch2, patch3], prop={'size': 60})
+    plt.savefig("plots/clustering_type_legend.png")
+
 
 
 if (__name__=="__main__"):
     # extract all necessary datapoints
-    names, groups, traces = unpack()
+    names, ion_types, groups, traces = unpack()
 
-    dim_reduction(names, groups, traces)
+    dim_reduction(names, ion_types, groups, traces)
+    create_legends()
