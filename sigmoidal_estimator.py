e937e2ef0bc3a344e459d6f5abf594b6cf2c0e4a
Devrim Celik <DevCel@hotmail.de>
2018-02-01 11:35:23 +0000

    fixed rmse, since i didnt normalize before. now looks at the rmse for every model and if the rmse is too high, we filter this channel (since it is probably not sigmoidal shaped)

--- sigmoidal_estimator.py
+++ sigmoidal_estimator.py
@@ -16,6 +16,7 @@ class Sigmoidal_Estimator():
         self.V = np.arange(-150, 151, 5)
 
 
+
     # master function
     def routine(self):
         # added bias for negative function
@@ -27,25 +28,32 @@ class Sigmoidal_Estimator():
         self.scale_est()
         # plotting
         #self.plot()
-
-
+        # calculate RMSE
         self.RMSE()
 
-        if (self.rmse > 2):
-            print("[!] Model({}) has RMSE of {}, please check!".format(self.name, self.rmse))
+        # check for outliers, probably non sigmoidal shaped
+        if (self.rmse > 0.1):
+            print("[!] Model({}) has RMSE of {}; it will be removed!".format(self.name, self.rmse))
+            self.plot()
+            return None, None, None
 
         return self.rate, self.midpoint, self.scale
+
 
 
     # calculate rate variable
     def rate_est(self):
         # check for "squishness"
-        max_val = np.max(self.trace)
-        min_val = np.min(self.trace)
-        # positive or negative
-        sign_dict = {"PS": 1, "NS": -1}
+        self.max_val = np.max(self.trace)
+        self.min_val = np.min(self.trace)
 
-        self.rate = sign_dict[self.group]*(max_val-min_val)
+        if self.group == "PS":
+            self.rate = (self.max_val-self.min_val)
+            self.b = self.min_val
+        elif self.group == "NS":
+            self.rate = -(self.max_val-self.min_val)
+            self.b = self.max_val
+
 
 
     # calculate midpoint variable
@@ -53,11 +61,12 @@ class Sigmoidal_Estimator():
         max_val = np.max(self.trace)
         min_val = np.min(self.trace)
 
-        midpoint_value = (max_val-min_val)/2
+        midpoint_value = (max_val-min_val)/2 + self.min_val
 
         # find corresponding voltage value
         idx = (np.abs(self.trace-midpoint_value)).argmin()
         self.midpoint = self.V[idx]
+
 
 
     # calculate scale variable by testing different value of scale
@@ -83,16 +92,18 @@ class Sigmoidal_Estimator():
         translated = self.calculate_values()
 
         plt.figure(figsize=(20,10))
-        plt.title(self.name+ "\nRMSE = " + str(self.rmse) + " Group: " + self.group + " M: " + str(self.midpoint) + "R: " + str(self.rate) + " S " +  str(self.scale))
+        plt.title(self.name+ "\nRMSE = " + str(self.rmse) + " Group: " + self.group + " M: " + str(self.midpoint) + "R: " + str(self.rate) + " S " +  str(self.scale) + " B " + str(self.b))
         plt.plot(self.V, self.trace, label="Simulation Trace", color="k", linewidth=4)
         plt.plot(self.V, translated, label="Translated Trace", color="r", linestyle="--")
         plt.legend()
         plt.show()
 
 
+
     # basic sigmoid function
     def sigmoid_function(self, V):
         return self.rate / (1 + np.exp( -(V - self.midpoint)/self.scale)) + self.b
+
 
 
     # return list of values over self.V
@@ -104,6 +115,7 @@ class Sigmoidal_Estimator():
         return translated
 
 
+
     # calculate RMSE
     def RMSE(self):
         # calculate values
@@ -113,7 +125,8 @@ class Sigmoidal_Estimator():
         for x, y in zip(self.trace, translated):
             self.rmse += (x-y)**2
 
-        self.rmse = np.sqrt(self.rmse)
+        self.rmse = np.sqrt(self.rmse/len(self.trace))
+
 
 
 if (__name__=="__main__"):
@@ -134,21 +147,17 @@ if (__name__=="__main__"):
         if (group == "PS" or group == "NS"):
             SE = Sigmoidal_Estimator(name, ion_type, group, trace)
             rate, midpoint, scale = SE.routine()
-
-
-            analysis_df.loc[iterator]["Name"] = name
-            analysis_df.loc[iterator]["Type"] = ion_type
-            analysis_df.loc[iterator]["Group"] = group
-            analysis_df.loc[iterator]["Trace"] = trace
-            analysis_df.loc[iterator]["Rate"] = rate
-            analysis_df.loc[iterator]["Midpoint"] = midpoint
-            analysis_df.loc[iterator]["Scale"] = scale
-
-            if SE.rmse > 2:
-                analysis_df.loc[iterator]["RMSE"] = 2
-            else:
+            # if flawed value, dont include
+            if not (rate == None and midpoint == None and scale == None):
+                analysis_df.loc[iterator]["Name"] = name
+                analysis_df.loc[iterator]["Type"] = ion_type
+                analysis_df.loc[iterator]["Group"] = group
+                analysis_df.loc[iterator]["Trace"] = trace
+                analysis_df.loc[iterator]["Rate"] = rate
+                analysis_df.loc[iterator]["Midpoint"] = midpoint
+                analysis_df.loc[iterator]["Scale"] = scale
                 analysis_df.loc[iterator]["RMSE"] = SE.rmse
 
-            iterator += 1
+                iterator += 1
 
     analysis_df.to_pickle("data/analysis_df.pickle")
