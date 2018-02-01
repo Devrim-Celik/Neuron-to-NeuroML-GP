34c0f791cabfe939c999bd13e37d06c09e378865
Devrim Celik <DevCel@hotmail.de>
2018-02-01 13:29:14 +0000

    added 'write_exception' function, which one can use once a bad data, wrongs size, only constant values, too high/low values, is detected to write them down in a text file. also moved the process of filtering too high/low values into 'extract_df.py'

--- auxiliary.py
+++ auxiliary.py
@@ -24,14 +24,6 @@ def unpack(file_name="extraced_df.pickle", only_sig=True, return_df=False):
         df = df[(df['Group']=="PS") | (df['Group']=="NS")]
         df.index = range(len(df[(df['Group']=="PS") | (df['Group']=="NS")]))
 
-    # filter out outliers
-    to_drop = []
-    for nr, element in enumerate(df["Trace"]):
-        if (max(element[0][0])>1) or (min(element[0][0])<0):
-            to_drop.append(nr)
-    df = df.drop(df.index[to_drop])
-    df.index = range(len(df))
-
     if return_df:
         return df
     else:
@@ -42,6 +34,7 @@ def unpack(file_name="extraced_df.pickle", only_sig=True, return_df=False):
 def save_fig(name, ion_type, group, trace):
 
     time = np.arange(-150, 151, 5)
+
     # remove parent directory
     # remove ending
     # remove instances of "/"
@@ -59,17 +52,45 @@ def save_fig(name, ion_type, group, trace):
 
 
 def generate_plots(names, types, groups, traces):
-    time = np.arange(-150, 151, 5)
-
     # check if group folder exist, if not create them
     group_folders = ["./plots/PS", "./plots/NS", "./plots/G", "./plots/F"]
+    ion_types = ["/Na", "/K", "/Ca"]
     for folder in group_folders:
         if not os.path.exists(folder):
             os.makedirs(folder)
+        for ion in ion_types:
+            if not os.path.exists(folder+ion):
+                os.makedirs(folder+ion)
 
     for nr in range(traces.shape[0]):
-        save_fig(names[nr], types[nr], groups[nr], traces[nr], time)
+        save_fig(names[nr], types[nr], groups[nr], traces[nr])
 
+
+def write_exception(name, nr, remove_path="./data/removed.txt"):
+    # check if text file is already set up, if not create it and write
+    # explanational line
+    if not os.path.exists(remove_path):
+        f = open(remove_path, "w+")
+        f.write("List of gating models to further inspect:\n\
+        - 1 indicates a constant value over the the whole value range\n\
+        - 2 indicates non-sigmoidal shape in some cases\n\
+        - 3 indicates wrong size of voltage values (!= 61)\n\
+        - 4 indicates too high(>1)/low(<0) values for the probability\n")
+        f.close()
+
+    # construct string, which should be added
+    add_string = name + "\t" + str(nr) + "\n"
+
+    # open to read and get list of lines
+    f = open(remove_path, "r+")
+    lines = f.read().strip().split()
+    f.close()
+
+    # check if string is already in there
+    if not (add_string in lines):
+        f = open(remove_path, "a+")
+        f.write(add_string)
+        f.close()
 
 
 if __name__=="__main__":
