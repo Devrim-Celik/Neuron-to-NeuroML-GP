06ef59f465707624ac5cff2ef0aa56deda404e36
Devrim Celik <DevCel@hotmail.de>
2018-02-01 13:30:26 +0000

    added filtering out processes using the newly implemented write_exception method. looks at size of data array, highest/lowest value, if behaviour is constant

--- extract_df.py
+++ extract_df.py
@@ -2,8 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from auxiliary import write_exception
 
-def get_gate_values(directory="./raw_data"):
+def get_gate_values(directory="./raw_data", remove_path="./data/removed.txt"):
+
+
+
 
     # get list with paths to all folders in the current directory
     folder_list = [x[0] for x in os.walk(directory) if ".mod" in x[0]]
@@ -32,47 +36,61 @@ def get_gate_values(directory="./raw_data"):
 
                         data = data.reshape(1,61)
 
-                        # group id, NOTE
-                        #   Assignment of the groups:
-                        #   - "PS": Positive Sigmoidal of form 1/(1+exp(-t))
-                        #   - "NS": Negative Sigmoidal of form -1/(1+exp(-t))
-                        #   - "G": Gaussian Distributed
-                        #   - "R": Rest --> Probably Screwed Data
+                        # filter out outliers
+                        if (np.amax(data)>1) or (np.amin(data)<0):
+                            write_exception(name, 4)
 
-                        # assign groups, given slope
-                        if (data[0,-1] - data[0,0]) < -0.1:
-                            group = "NS"
-                        elif (data[0,-1] - data[0,0]) > 0.1:
-                            group = "PS"
-
-                        # very similiar start at end point, if max == min:
-                        # --> screwed data
                         else:
-                            if (np.max(data) != np.min(data)):
-                                group = "G"
+                            # group id, NOTE
+                            #   Assignment of the groups:
+                            #   - "PS": Positive Sigmoidal of form 1/(1+exp(-t))
+                            #   - "NS": Negative Sigmoidal of form -1/(1+exp(-t))
+                            #   - "G": Gaussian Distributed
+                            #   - "R": Rest --> Probably Screwed Data
+
+                            # assign groups, given slope
+                            if (data[0,-1] - data[0,0]) < -0.1:
+                                group = "NS"
+                            elif (data[0,-1] - data[0,0]) > 0.1:
+                                group = "PS"
+
+                            # very similiar start at end point, if max == min:
+                            # --> screwed data
                             else:
-                                group = "F"
+                                if (np.max(data) != np.min(data)):
+                                    group = "G"
+                                else:
+                                    group = "F"
+                                    # add to removed list, since this grouping
+                                    # most likely indicates a problem with the
+                                    # value generation of the mod file
+                                    write_exception(name, 1)
 
-                        # find out which ion type
-                        if "/K/" in name:
-                            type_ion = "K"
-                        elif "/Na/" in name:
-                            type_ion = "Na"
-                        elif "/Ca/" in name:
-                            type_ion = "Ca"
-                        else:
-                            print("[-] Type not found")
+                            # find out which ion type
+                            if "/K/" in name:
+                                type_ion = "K"
+                            elif "/Na/" in name:
+                                type_ion = "Na"
+                            elif "/Ca/" in name:
+                                type_ion = "Ca"
+                            else:
+                                print("[-] Type not found")
 
-                        if not np.isnan(data).any():
-                            # add values to dataframe
-                            total_df.loc[index_counter]["Name"] = name
-                            total_df.loc[index_counter]["Type"] = type_ion
-                            total_df.loc[index_counter]["Group"] = group
-                            total_df.loc[index_counter]["Trace"] = pd.Series([data])
+                            if not np.isnan(data).any():
+                                # add values to dataframe
+                                total_df.loc[index_counter]["Name"] = name
+                                total_df.loc[index_counter]["Type"] = type_ion
+                                total_df.loc[index_counter]["Group"] = group
+                                total_df.loc[index_counter]["Trace"] = pd.Series([data])
 
 
 
-                            index_counter += 1
+                                index_counter += 1
+
+                    else:
+                        write_exception(name, "3")
+
+
 
     # since there wasnt another way that give more indexes than we need
     # at the start, of dynamically locating them, remove all empty rows
